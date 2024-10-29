dd2c50a35e8407623d47f045cfe96db0f6b099ca
shinkarom <shinkarom@gmail.com>
2024-10-29 20:40:16 +0200

    Use libretro's button mapping constants

--- src/common.hpp
+++ src/common.hpp
@@ -1,11 +1,25 @@
 #pragma once
 
 #include <cstdint>
+#include "libretro.h"
 
 extern void (*wh_callback)(int w, int h);
 extern void (*syscall_callback)(int address, int value);
 
-constexpr int buttonMapping[12] = {4, 5, 6, 7, 2, 3, 8, 0, 9, 1, 10, 11};
+constexpr int buttonMapping[12] = {
+ RETRO_DEVICE_ID_JOYPAD_UP,
+ RETRO_DEVICE_ID_JOYPAD_DOWN, 
+ RETRO_DEVICE_ID_JOYPAD_LEFT,
+ RETRO_DEVICE_ID_JOYPAD_RIGHT,
+ RETRO_DEVICE_ID_JOYPAD_SELECT,
+ RETRO_DEVICE_ID_JOYPAD_START,
+ RETRO_DEVICE_ID_JOYPAD_A,
+ RETRO_DEVICE_ID_JOYPAD_B, 
+ RETRO_DEVICE_ID_JOYPAD_X,
+ RETRO_DEVICE_ID_JOYPAD_Y,
+ RETRO_DEVICE_ID_JOYPAD_L,
+ RETRO_DEVICE_ID_JOYPAD_R
+ };
 constexpr int numButtons = 12;
 
 constexpr auto maxScreenWidth = 256;
